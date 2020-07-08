5cc5a8913a94ea3a637dc1a790bf6fcea3e5a638
Kopilov <aleksandr.kopilov@bia-tech.ru>
2020-07-08 18:48:07 +0300

    reading

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "arrow/ipc/api.h"
 
 
-int main() {
+int main1() {
     std::cout << "Hello Arrow!" << std::endl;
     arrow::Int64Builder builder;
     // Make place for 8 values in total
@@ -41,10 +41,24 @@ int main() {
 }
 
 
-int main2() {
+int main() {
     std::cout << "Hello Arrow2!" << std::endl;
     arrow::Result<std::shared_ptr<arrow::io::ReadableFile>> input = arrow::io::ReadableFile::Open("test");
-//    (*input)->Read()
+    std::shared_ptr<arrow::ipc::feather::Reader> reader = (*arrow::ipc::feather::Reader::Open(
+                                                               *(arrow::io::MemoryMappedFile::Open("test", arrow::io::FileMode::READWRITE)))
+                                                           );
+    std::shared_ptr<arrow::Table> tableTarget;
+    reader->Read(&tableTarget);
+    std::shared_ptr<arrow::Array> array = tableTarget->column(0)->chunk(0);
+    std::shared_ptr<arrow::Int64Array> int64_array = std::static_pointer_cast<arrow::Int64Array>(array);
+    std::cout << (*int64_array).Value(0) << std::endl;
+    std::cout << int64_array->Value(1) << std::endl;
+    std::cout << int64_array->Value(2) << std::endl;
+    std::cout << int64_array->Value(3) << std::endl;
+    std::cout << int64_array->Value(4) << std::endl;
+    std::cout << int64_array->Value(5) << std::endl;
+    std::cout << int64_array->Value(6) << std::endl;
+    std::cout << int64_array->Value(7) << std::endl;
     return 0;
 }
 
